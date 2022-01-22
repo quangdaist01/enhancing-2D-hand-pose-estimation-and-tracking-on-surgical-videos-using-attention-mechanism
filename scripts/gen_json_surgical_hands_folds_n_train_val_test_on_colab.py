847765a6011f2b7c23139e795077d0bfcd75db51
Lov <quangdaist01@gmail.com>
2022-01-22 18:48:19 +0700

    comment sample code

--- scripts/gen_json_surgical_hands_folds_n_train_val_test_on_colab.py
+++ scripts/gen_json_surgical_hands_folds_n_train_val_test_on_colab.py
@@ -57,9 +57,9 @@ val_videos = ['QokL8kNka9g', '19P6qybcoUE', '09116df3238', 'a7d419fca90']
 test_videos = ['e45vsP9CM2c', '01560685f81', '68e2d14b311', '8jXjg4uaES0']
 train_videos = [video for video in all_videos if video not in [*val_videos, *test_videos]]
 
-val_videos = [val_videos[0]]
-test_videos = [test_videos[0]]
-train_videos = [train_videos[0]]
+# val_videos = [val_videos[0]]
+# test_videos = [test_videos[0]]
+# train_videos = [train_videos[0]]
 
 ##
 splits = ['train', 'val', 'test']
